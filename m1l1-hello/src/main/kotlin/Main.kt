65c38c5dee65daf7b9a806fa7edf9da0cbb6a2b2
Goga <xultraboix@gmail.com>
2023-07-10 18:03:54 +0300

    home-2

--- m1l1-hello/src/main/kotlin/Main.kt
+++ m1l1-hello/src/main/kotlin/Main.kt
@@ -1,6 +1,5 @@
 
-fun main(args: Array<String>){
-    println("Hello world!")
-
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    val intsList = listOf<Int>(1, 2, 3, 4, 5, 6, 7)
+    for (i in 0..11) println(intsList[i])
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@
 rootProject.name = "ok-marketplace-practice"
 
 include("m1l1-hello")
+include("m1l2")
 
 pluginManagement    {
     val kotlinVersion: String = "1.8.20"
@@ -10,3 +11,4 @@ pluginManagement    {
         kotlin("jvm") version kotlinVersion apply false
     }
 }
+
